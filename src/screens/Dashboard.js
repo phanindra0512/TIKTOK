69c07bd24ea8ba9c0eb75f4acbb9a81182c53808
NarendraReddy <nareereddykp6@gmail.com>
2020-08-09 14:30:36 +0530

    changes in login screen

---
+++ src/screens/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+
+function Dashboard({ navigation }) {
+
+
+
+    
+
+    return (
+        <View style={{ flex: 1, justifyContent:'center'}}>
+           <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+export default Dashboard
---
+++ src/screens/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, } from 'react-native'
+import {  Button } from 'react-native-paper'
+
+function Login({ navigation }) {
+
+   
+
+    return (
+        <View style={{ flex: 1,alignItems:'center',justifyContent:'center' }}>
+           <Text>Login Screen</Text>
+            <Button mode='contained' onPress={()=>navigation.navigate('Dashboard')}>Login</Button>
+           <Text style={{fontSize:20,fontWeight:'bold'}} onPress={()=>navigation.navigate('Signup')}>Signup</Text> 
+        </View>
+    )
+}
+
+export default Login
